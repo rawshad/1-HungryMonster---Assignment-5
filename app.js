6fa3663dfce5bf3e84141a9a7f29b709378d1e03
rawshad <rawshad@outlook.com>
2021-02-09 21:14:55 +0600

    show ingredients functionality added

--- app.js
+++ app.js
@@ -21,11 +21,31 @@ const displayFood = foodItem => {
         const foodDiv = document.createElement('div');
         foodDiv.className = 'col-md-3';
         foodDiv.innerHTML = `
-            <figure class="figure">
+            <figure onclick="foodDetails('${items.strMealThumb}','${items.strMeal}', '${items.strIngredient1}', '${items.strIngredient2}', '${items.strIngredient3}', '${items.strIngredient4}', '${items.strIngredient5}', '${items.strIngredient6}')" class="figure">
                 <img src="${items.strMealThumb}" class="figure-img img-fluid rounded" alt="...">
                 <figcaption class="figure-caption text-center food-caption">${items.strMeal}</figcaption>
             </figure>
         `
         foodContainer.appendChild(foodDiv);
     })
+}
+
+const foodDetails = (thumbnail, foodName, ing1, ing2, ing3, ing4, ing5, ing6) => {
+    // console.log(foodName, ing1, ing2, ing3, ing4, ing5, ing6);
+    const ingredientCard = document.getElementById("ingredients-card");
+    ingredientCard.innerHTML =`
+            <img src="${thumbnail}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${foodName}</h5>
+                <p class="card-text fw-bold">Ingredients</p>
+                <ul class="list-group">
+                <li class="list-group-item">${ing1}</li>
+                <li class="list-group-item">${ing2}</li>
+                <li class="list-group-item">${ing3}</li>
+                <li class="list-group-item">${ing4}</li>
+                <li class="list-group-item">${ing5}</li>
+                <li class="list-group-item">${ing6}</li>
+                </ul>
+            </div>
+    `
 }
